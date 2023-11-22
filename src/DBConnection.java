a2cc9e5a197f1061f881d166e8e844c2a190b87d
Stefan <stu2101261004@uni-plovdiv.com>
2023-11-22 11:30:40 +0200

    m

---
+++ src/DBConnection.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    static Connection conn = null;
+
+    static Connection getConnection() {
+        try {
+            Class.forName("org.h2.Driver");
+
+            //При мен работи. Не съм я вързал към GUI. Трябва да си промениш пътя
+
+            conn = DriverManager.getConnection("jdbc:h2:C:\\java\\Proekt_Razpredeleni\\Database", "sa", "123");
+        } catch (ClassNotFoundException | SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
